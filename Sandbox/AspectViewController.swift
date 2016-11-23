8ec650bac19dabdca6444194d730caa6979cdcd7
gangelo <web.gma@gmail.com>
2016-11-23 04:28:51 -0500

    Initial commit

---
+++ Sandbox/AspectViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AspectViewController.swift
+//  Sandbox
+//
+//  Created by Gene M. Angelo  Jr. on 11/22/16.
+//  Copyright © 2016 Mohojo Werks LLC. All rights reserved.
+//
+
+import UIKit
+
+class AspectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   @IBAction func onCancelPressed(_ sender: UIBarButtonItem) {
+      self.dismiss(animated: true, completion: nil)
+   }
+
+}
